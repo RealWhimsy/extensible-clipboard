a25e55e0acf0377c4ccf52ad140f99913b1f57d4
Matthias Roesl <matthias.roesl@stud.uni-regensburg.de>
2019-02-04 16:49:37 +0100

    Added draft for popup.

--- src/chrome-plugin/clipboard_api.js
+++ src/chrome-plugin/clipboard_api.js
@@ -12,22 +12,6 @@ var clipboardApi = (function(){
         }
     }
 
-    function onClipsGet(){
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200){
-            console.log(this.response)
-            let clipList = document.getElementById('clipList');
-            for (var i = 0; i < this.response.length; i++){
-                r = this.response[i].data.toString();
-                console.log(r)
-                r = r.replace('<a ', '<a target="_blank" ')
-                r = r.replace('</a>', 'Item</a>')
-                console.log(r)
-                $('#clipList').append('<li>'+r+'</li>')
-            }
-        }
-        
-    }
-
     function saveClip(data, mimetype, src_url, src_app){
         console.log(data)
         xhr = requestBuilder.buildRequest('POST', BASE_CLIP_URL, onClipSent);
@@ -39,10 +23,11 @@ var clipboardApi = (function(){
         requestBuilder.sendRequest(xhr, send_data);
     }
 
-    function getAllClips(){
-        console.log('getting clips')
-        xhr = requestBuilder.buildRequest('GET', BASE_CLIP_URL, onClipsGet);
-        requestBuilder.sendRequest(xhr)
+    function getAllClips(context, callback){
+        $.ajax(BASE_CLIP_URL, {
+            contentType: 'application/json',
+            context: context,
+        }).done(callback)
     }
 
     that.saveClip = saveClip;
@@ -51,6 +36,6 @@ var clipboardApi = (function(){
 
     return that;
 
-})();
+}());
 
 
--- src/chrome-plugin/popup.js
+++ src/chrome-plugin/popup.js
@@ -1,8 +1,46 @@
-function onSyncButtonClick(){
-    console.log('blub')
-    clipboardApi.getAllClips()
-}
+var app = (function(){
+    var that = {};
+    var syncButton = null;
+    var clipTemplate = null;
+    var $table = null;
 
-let syncButton = $('#syncButton')
-console.log(syncButton)
-syncButton.click(onSyncButtonClick)
+    function onSyncButtonClick(){
+        $("#clipList").empty()
+        clipboardApi.getAllClips(app, onClipsGet)
+    }
+
+    function onClipsGet(data, textStatus, jqXHR){
+        if (jqXHR.status === 200 ){
+            var clips = [];
+            var clipList = document.getElementById('clipList');
+            for (var i = 0; i < data.length; i++){
+                clip = {};
+                clip.clipText = data[i].data.toString()
+                clip.clipType = data[i].mimetype
+                if (data[i].mimetype === 'text/html'){
+                    a = $(clip.clipText).attr('href')
+                    clip.clipText = clip.clipText.replace('<a ', '<a target="_blank" ')
+                    clip.clipText = clip.clipText.replace('</a>', a + '</a>')
+                }
+                clips.push(clip);
+            }
+            var _new = this.clipTemplate({clips: clips})
+            this.$table.append(_new)
+        }
+    }
+
+    function init(){
+        this.clipTemplate = Handlebars.compile(
+            $("#clipTemplate").html()
+        )
+        this.$table = $('#clipTable tbody')
+
+        this.syncButton = $('#syncButton')
+        this.syncButton.click(onSyncButtonClick)
+    }
+
+    that.init = init;
+    that.loadClips = onSyncButtonClick
+
+    return that;
+}());

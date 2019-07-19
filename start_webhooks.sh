1bd16d099cc6ae46b1b21520dd86a8a38e62dc54
FelixRDL <felix1.riedl@stud.uni-regensburg.de>
2019-07-19 07:10:48 -0400

    Add webhook input parameter

--- start_webhooks.sh
+++ start_webhooks.sh
@@ -1,1 +1,2 @@
-python3 ./src/webhooks/main.py -p 6000 -d http://localhost:6000/ -c http://localhost:5000/
+SERVER_ADDR=$1
+python3 ./src/webhooks/main.py -p 6000 -d http://localhost:6000/ -c $SERVER_ADDR

de022aaf082ad23c89a78eeccec307da2febc4e6
FelixRDL <felix1.riedl@stud.uni-regensburg.de>
2019-07-22 15:31:48 +0100

    Implement installation process for client

--- start_clipboard_service.sh
+++
@@ -1,5 +0,0 @@
-SERVER_ADDR=$1
-MY_IP=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
-MY_IP="http://$MY_IP:5555/"
-echo $MY_IP
-python3 ./src/clipboard_server/main.py --port 5555 --domain $MY_IP --clipserver=$SERVER_ADDR --sync-clipboard True

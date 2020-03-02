c2c4dfcf6ea4210945997c8ddfc4b4abb77e35ab
felix <felix@debian>
2020-03-02 12:07:08 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 sudo apt install python3-venv python3-dev binutils
 cd ./servers
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 fbs freeze

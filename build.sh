f412f2826f8d780cf65092abd129eb0a8ea7c22c
FelixRDL <felix1.riedl@stud.uni-regensburg.de>
2020-03-02 03:31:43 -0500

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+sudo apt install python3-venv python3-dev binutils
+cd ./servers
+python3 -m venv venv
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+fbs freeze

3eca2eb5c84ed87450c21c01eb1ec7cf86225eda
FelixRDL <felix1.riedl@stud.uni-regensburg.de>
2020-03-16 10:12:42 -0400

    Adapt wipe method in testing to new table names #30

--- servers/src/main/python/tests/t_util.py
+++ servers/src/main/python/tests/t_util.py
@@ -4,6 +4,6 @@ import os
 def wipe():
     path = os.path.expanduser('~/clip_collection.db')
     conn = sqlite3.connect(path)
-    conn.execute('DELETE FROM clips')
-    conn.execute('DELETE FROM clipboards')
+    conn.execute('DELETE FROM clip')
+    conn.execute('DELETE FROM clipboard')
     conn.commit()
